b69cb9269548775c0f0d469ba53cb408c010ea87
kgorz <gorzynskikrzysztof@gmail.com>
2015-06-17 22:19:56 +0200

    Init commit

---
+++ naive.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# 1) NAUKA
+
+# 1.1) Otwórz pliki do nauki
+
+# 1.2) Pobierz wszystkie słowa z train_spam_and_ham
+#      Osobna baza słów dla train_spam i train_ham
+#      Zlicz wystąpienia tych słów
+#      Zlicz pliki (200) i na tej podstawie wylicz szanse
+#      http://www.statsoft.pl/textbook/stathome_stat.html?http%3A%2F%2Fwww.statsoft.pl%2Ftextbook%2Fstnaiveb.html
+
+
+# 2) Jedziemy z koksem
+
+# 2.1) Otwórz plik z katalogu test
+
+# 2.2) Pobierz wszystkie słowa z pliku
+#      Może się mylę ale prawdopodobieństwo a priori = 1,
+#      bo na wstępie nie możemy sklasyfikować danych
+
+# 2.3) Oblicz prawdopodobieństwo a posteriori dla danego pliku
+#      tzn. jeśli a posteriori słów częście występujących w train_spam
+#      jest większe sklasyfikuj jako spam
+
+
